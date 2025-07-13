b0eaf9bc9dfc4b61b3c22f88ab707a908e01793f
BearForOdin <selasidzeamesi82@gmail.com>
2025-07-13 16:29:34 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import Header from "./components/Header/header";
+import Hero from "./components/Hero/hero";
+import './App.css'
+import Companies from "./Companies/Companies";
+import Residencies from "./components/Residencies/Residencies";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <div>
+        <div className="white-gradient"/>
+        <Header/>
+      <Hero/>
+      </div>
+      <Companies/>
+      <Residencies/>
+    </div>
   );
 }
 
