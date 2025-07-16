4fab28b53839d6d2be71da42354807d939c0f91f
BearForOdin <selasidzeamesi82@gmail.com>
2025-07-15 17:59:42 -0700

    Changes_1.0

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
-import "swiper/css"
-import "./Residencies.css"
-import data from '../../utils/slider.json'
+import React from 'react';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import "swiper/css";
+import "./Residencies.css";
+import data from '../../utils/slider.json';
+import { sliderSettings } from '../../utils/common';
 
-const residencies = () => {
+const Residencies = () => {
   return (
     <section className="r-wrapper">
       <div className="paddings innerWidth r-container">
@@ -13,7 +14,7 @@ const residencies = () => {
           <span className='primaryText'>Popular Residencies</span>
         </div>
 
-        <Swiper>
+        <Swiper {...sliderSettings}>
           {
             data.map((card, i)=>(
               <SwiperSlide key={i}>
@@ -30,10 +31,23 @@ const residencies = () => {
               </SwiperSlide>
             ))
           }
+
+          <SliderButtons/>
         </Swiper>
+
       </div>
     </section>
   );
 };
 
-export default residencies
+export default Residencies;
+
+const SliderButtons = () => {
+  const swiper = useSwiper();
+  return (
+    <div className='r-button'>
+      <button onClick={()=> swiper.slidePrev()}>&lt;</button>
+      <button onClick={()=> swiper.slideNext()}>&gt;</button>
+    </div>
+  );
+};
